e01032206b0adcad1e1a95f213861ccb92b5aac8
Simon <simon.baer1@ost.ch>
2025-05-13 12:58:01 +0200

    Changed src folder to weather

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .analyzer import WeatherAnalyzer
-from .fetcher import WeatherDataFetcher
-from .plotter import WeatherPlotter
-from .singleton import Singleton
-from .models import LocationWeatherData, TemperatureData, PrecipitationData, WindData
